9160cca059ae9dba3bc8ffca3f7dc0f8305ebf55
LebrusX <trolebrus@gmail.com>
2021-03-11 22:10:16 +0300

    ok

--- src/test/java/stringer/StringConcatenatorTest.java
+++ src/test/java/stringer/StringConcatenatorTest.java
@@ -1,8 +1,12 @@
 package stringer;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,11 +28,27 @@ class StringConcatenatorTest {
     @Test
     @DisplayName("throws NullPointerException (why??:D)")
     void throwsException() {
-        assertAll("throws",
-                () -> assertThrows(NullPointerException.class, () -> StringConcatenator.concat(null, "b", 0)),
-                () -> assertThrows(NullPointerException.class, () -> StringConcatenator.concat(null, null, 12))
-        );
+        assertThrows(NullPointerException.class, () -> StringConcatenator.concat(null, "b", 0));
     }
 
+    @ParameterizedTest
+    @DisplayName("doesn't throws NullPointerException")
+    @EmptySource
+    @ValueSource(strings = {"\nd", " ", "\tr", "a"})
+    void parametrizedTest1(String s) {
+        assertDoesNotThrow(() -> StringConcatenator.concat(s, "vvv", 12));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"a,b,1,ab", "d,f,3,dfff", "one,f,-12,one"})
+    void parametrizedTest2(String s1, String s2, int n, String expected) {
+        String actualValue = StringConcatenator.concat(s1, s2, n);
+        assertEquals(expected, actualValue);
+    }
+
+    @RepeatedTest(10)
+    void repeatedTest() {
+        assertEquals("aabbbbbbbbbb", StringConcatenator.concat("aa", "b", 10));
+    }
 
 }
